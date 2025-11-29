faaa80cbe88c831d05f6d16c9525c1d3d2e104f7
KimJinwoo2198 <contact@kimjinwoo.me>
2025-11-29 10:36:29 +0900

    Update WebSocket route for FoodChatConsumer to allow optional trailing slash in URL pattern.

--- vision/routing.py
+++ vision/routing.py
@@ -3,7 +3,7 @@ from django.urls import re_path
 from vision.consumers import FoodChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r"^ws/vision/foods/chat/$", FoodChatConsumer.as_asgi()),
+    re_path(r"^ws/vision/foods/chat/?$", FoodChatConsumer.as_asgi()),
 ]
 
 
